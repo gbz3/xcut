0ba08a33f9c5562ef59faff18adb90a5d21aef41
gbz3 <gebizo3@gmail.com>
2018-04-15 09:56:56 +0900

    矩形切り出しに対応

--- main.go
+++ main.go
@@ -6,16 +6,19 @@ import (
   "io/ioutil"
   "os"
   "regexp"
+  "strconv"
 
   "github.com/tealeg/xlsx"
 )
 
 var fs string
+var fc string
 var fk string
 var fa bool
 
 func init() {
   flag.StringVar( &fs, "s", "", "specified sheet name" )
+  flag.StringVar( &fc, "c", "", "cut off data" )
   flag.StringVar( &fk, "k", "", "keyword for search" )
   flag.BoolVar( &fa, "a", false, "searching all" )
 }
@@ -41,15 +44,43 @@ func main() {
     os.Exit( 0 )
   }
 
-  if _, ok := book.Sheet[ fs ]; !ok {
+  // evaluate fs
+  sheet, ok := book.Sheet[ fs ]
+  if !ok {
     fmt.Println( "sheet not found." )
     os.Exit( 1 )
   }
 
-  for i, row := range book.Sheet[ fs ].Rows {
-    for j, cell := range row.Cells {
+  // evaluate fc
+  var cmin, cmax, rmin, rmax int = 0, sheet.MaxRow, 0, sheet.MaxCol
+  if len( fc ) > 0 {
+    re := regexp.MustCompile( `^(([A-Z]+)([0-9]*))?:(([A-Z]+)([0-9]*))?$` )
+    if re.MatchString( fc ) {
+//fmt.Println( re.ReplaceAllString( fc, "match1=[$2] match2=[$3] match3=[$5] match4=[$6]" ) )
+      if re.ReplaceAllString( fc, "$1" ) != "" {
+        cmin = xlsx.ColLettersToIndex( re.ReplaceAllString( fc, "$2" ) )
+        rmin, _ = strconv.Atoi( re.ReplaceAllString( fc, "$3" ) )
+        rmin--
+      }
+      if re.ReplaceAllString( fc, "$4" ) != "" {
+        cmax = xlsx.ColLettersToIndex( re.ReplaceAllString( fc, "$5" ) )
+        rmax, _ = strconv.Atoi( re.ReplaceAllString( fc, "$6" ) )
+        rmax--
+      }
+    } else {
+      fmt.Println( "invalid axis." )
+      os.Exit( 1 )
+    }
+  }
+//fmt.Printf( "cmin=%d cmax=%d rmin=%d rmax=%d\n", cmin, cmax, rmin, rmax )
+
+  // evaluate fk, fa
+  for r, row := range sheet.Rows {
+    if r < rmin || rmax < r { continue }
+    for c, cell := range row.Cells {
+      if c < cmin || cmax < c { continue }
       if len( fk ) > 0 && regexp.MustCompile( fk ).MatchString( cell.String() ) {
-        fmt.Printf( "%s%d\tText=[%s]\n", xlsx.ColIndexToLetters( j ), i, cell.String() )
+        fmt.Printf( "%s%d\tText=[%s]\n", xlsx.ColIndexToLetters( c ), r+1, cell.String() )
         if fa == false {
           os.Exit( 0 )
         }
